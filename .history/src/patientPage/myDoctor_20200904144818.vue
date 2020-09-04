b5894d5a59fe1c9440abb8d4712c368865df90c1
wcy1997 <2419279033@qq.com>
2020-09-04 14:56:46 +0800

    myDoctor [001]
    myDoctor [001]

--- .history/src/patientPage/myDoctor_20200904144818.vue
+++ .history/src/patientPage/myDoctor_20200904144818.vue
@@ -1,22 +1,18 @@
 <template>
 <div class="myDoctor">
     <div class="back" @click="goBack">返回上一级</div>
-    <div class="search">
-        <input type="text" placeholder="输入医生名字" v-model="name" @input="getDoctorByTel" style="border:1px solid #e9e9e9;" />
-    </div>
+    <div class="search"><input type="text" placeholder="输入医生名字" v-model="name" @input="getDoctorByTel" style="border:1px solid #e9e9e9;"></div>
     <div class="xian"></div>
     <div class="tuijian">
         <div v-for="(item,index) in xz" :key="index" @click="goDetail(item.doctorId)">
-            <img :src="item.doctorPhoto" alt />
+            <img :src="item.doctorPhoto" alt="">
             <div>{{item.doctorName}} {{item.doctorLevelName}}</div>
             <div>{{item.doctorHospitalName}} {{item.departmentName}}</div>
-            <div>
-                <span v-for="(item,index) in item.list_adept||item.doctorAdept" :key="index">{{item.medicineSubjectName}}</span>
-            </div>
+            <div><span v-for="(item,index) in item.list_adept||item.doctorAdept" :key="index">{{item.medicineSubjectName}}</span></div>
         </div>
     </div>
-    <img src="../assets/img/LODING.gif" alt class="loading" v-if="loadUp" />
-    <img class="zanwuxinxi" v-show="ifShow" src="../assets/img/zanwuxinxi.png" />
+    <img src="../assets/img/LODING.gif" alt="" class="loading" v-if="loadUp">
+    <img class="zanwuxinxi" v-show="ifShow" src='../assets/img/zanwuxinxi.png' />
 </div>
 </template>
 
@@ -24,38 +20,40 @@
 import {
     doctorList,
     getDoctorByTel
-} from "api/patient";
-import HeadTop from "base/header/header";
-import router from "../router";
-import cookie from "js-cookie";
+} from "api/patient"
+import HeadTop from 'base/header/header'
+import router from '../router'
+import cookie from "js-cookie"
 export default {
     name: "myDoctor",
     data() {
         return {
-            token: "",
-            patientId: "",
+            token: '',
+            patientId: '',
             xz: [],
-            name: "",
+            name: '',
             loadUp: true,
-            ifShow: false,
-        };
+            ifShow: false
+        }
     },
     created() {
-        this.token = cookie.get("token");
-        this.patientId = cookie.get("patientId");
+        this.token = cookie.get("token")
+        this.patientId = cookie.get("patientId")
         this.doctorList();
     },
-    computed: {},
+    computed: {
+
+    },
 
     methods: {
         goBack() {
-            this.$router.go(-1);
+            this.$router.go(-1)
         },
         doctorList() {
             let list = {
                 token: this.token,
-                patientId: this.patientId,
-            };
+                patientId: this.patientId
+            }
             doctorList(list).then((res) => {
                 //console.log(res);
                 if (res.type == true) {
@@ -64,42 +62,45 @@ export default {
                     if (res.data.length == 0) {
                         this.ifShow = true;
                     } else {
-                        this.ifShow = false;
+                        this.ifShow = false
                     }
                 }
-            });
+
+            })
         },
         getDoctorByTel() {
             let list = {
                 token: this.token,
                 patientId: this.patientId,
-                doctorTel: this.name,
-            };
+                doctorTel: this.name
+            }
             getDoctorByTel(list).then((res) => {
                 //console.log(res);
                 if (res.type == true) {
                     this.xz = res.data;
                 } else {
                     // this.getDoctorByTel();
-                    this.$toast("查询失败");
+                    this.$toast('查询失败')
                 }
-            });
+            })
         },
         goDetail(doctorId) {
             this.$router.push({
-                path: "/yishengXq",
+                path: '/yishengXq',
                 query: {
-                    doctorId: doctorId,
-                },
-            });
+                    doctorId: doctorId
+                }
+            })
         },
     },
-    components: {},
-};
+    components: {
+
+    }
+}
 </script>
 
 <style lang="less" scoped>
-@import "../assets/less/base.less";
+@import '../assets/less/base.less';
 
 .myDoctor {
     width: 100%;
@@ -107,6 +108,7 @@ export default {
 
 .back {
     width: 26.668%;
+    ;
     height: 0.76rem;
     background: #00afc2;
     border-radius: 0.13rem;
@@ -120,20 +122,21 @@ export default {
 
 .xian {
     width: 100%;
-    height: 0.1rem;
+    height: 10px;
     background: #efefef;
     clear: both;
 }
 
 .search {
-    width: 62.93%;
+    width: 468px;
     float: left;
-    height: 0.76rem;
-    border: 0.01rem solid #e9e9e9;
-    border-radius: 0.38rem;
-    line-height: 0.76rem;
-    margin-top: 0.15rem;
-    margin-bottom: 0.15rem;
+    height: 76px;
+    border: 1px solid #e9e9e9;
+    ;
+    border-radius: 38px;
+    line-height: 76px;
+    margin-top: 15px;
+    margin-bottom: 15px;
 
     input {
         width: 100%;
@@ -178,7 +181,7 @@ export default {
             }
 
             &:nth-of-type(2) {
-                color: #bababa;
+                color: #BABABA;
                 padding-top: 16px;
                 height: 25px;
                 line-height: 30px;
@@ -203,7 +206,7 @@ export default {
 }
 
 .zanwuxinxi {
-    width: 3.3rem;
+    width: 3.30rem;
     height: 4.17rem;
     display: block;
     margin: 0 auto;
